aa338ce3cecc5567047987b6af3b22410e7ca22c
HARSH SAINI <139774512+sainiharshsaini@users.noreply.github.com>
2025-11-18 16:37:21 +0530

    Added Convex Package

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,15 +1,20 @@
-import { add } from "@workspace/math/add"
-import { Button } from "@workspace/ui/components/button"
-import { Input } from "@workspace/ui/components/input"
+"use client";
+
+import { useMutation, useQuery } from "convex/react"
+import { api } from "@workspace/backend/_generated/api"
+import { Button } from "@workspace/ui/components/button";
 
 export default function Page() {
+  const users = useQuery(api.users.getMany);
+  const addUser = useMutation(api.users.addUser);
+
   return (
-    <div className="flex items-center justify-center min-h-svh">
-      <div className="flex flex-col items-center justify-center gap-4">
-        <h1 className="text-2xl font-bold">Hello World</h1>
-        <Button size="sm" className="bg-red-500">Button</Button>
-        <p>{add(2,3)}</p>
-        <Input />
+    <div className="flex flex-col items-center justify-center min-h-svh">
+      <h2>
+        {JSON.stringify(users, null, 2)}
+      </h2>
+      <div>
+        <Button onClick={() => addUser()}>Add User</Button>
       </div>
     </div>
   )
---
+++ packages/backend/convex/users.ts
@@ -0,0 +1,21 @@
+import { mutation, query } from "./_generated/server";
+
+export const getMany = query({
+    args: {},
+    handler: async (ctx) => {
+        const users = await ctx.db.query("users").collect();
+
+        return users;
+    }
+})
+
+export const addUser = mutation({
+    args: {},
+    handler: async (ctx) => {
+        const userId = await ctx.db.insert("users", {
+            name: "Harsh Engineer",
+        })
+
+        return userId;
+    }
+})
