3a229691425f17d21622ee6b9dd4090a293faa45
HARSH SAINI <139774512+sainiharshsaini@users.noreply.github.com>
2025-11-26 23:00:59 +0530

    Added Sentry

--- packages/backend/convex/users.ts
+++ packages/backend/convex/users.ts
@@ -2,7 +2,7 @@ import { mutation, query } from "./_generated/server";
 
 export const getMany = query({
     args: {},
-    handler: async (ctx) => {
+    handler: async (ctx: any) => {
         const users = await ctx.db.query("users").collect();
 
         return users;
@@ -11,13 +11,15 @@ export const getMany = query({
 
 export const addUser = mutation({
     args: {},
-    handler: async (ctx) => {
+    handler: async (ctx: any) => {
         const identity = await ctx.auth.getUserIdentity();
         if (identity === null) throw new Error("Not authenticated");
 
         const orgId = identity.orgId as string;
 
         if (!orgId) throw new Error("Missing organization");
+
+        throw new Error("Tracking test");
 
         const userId = await ctx.db.insert("users", {
             name: "Harsh Engineer",
